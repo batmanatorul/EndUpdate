7e01d29ebc4ff839f69cdfebfbd55239387e8208
batmanatorul <batmanatorul@gmail.com>
2020-12-27 23:07:49 +0200

    v1

---
+++ src/main/java/com/batmanatorul/endupdate/EndUpdate.java
@@ -0,0 +1,11 @@
+package com.batmanatorul.endupdate;
+
+
+import net.fabricmc.api.ModInitializer;
+
+public class EndUpdate implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
